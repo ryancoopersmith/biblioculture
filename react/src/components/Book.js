800b6af8c3d1e5fb0742c478b6463c439f879486
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-04-11 18:06:06 -0400

    Add book component

--- react/src/components/Book.js
+++ react/src/components/Book.js
@@ -3,12 +3,56 @@ import React, { Component } from 'react';
 class Book extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      sites: []
+    }
+    this.getSites = this.getSites.bind(this);
+  }
+
+  getSites() {
+    fetch('http://localhost:3000/api/v1/sites.json', {
+      credentials: 'same-origin'
+      }).then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+              error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(body => {
+        this.setState({ sites: body });
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
+  componentDidMount() {
+    this.getSites();
   }
 
   render() {
+    let sites = this.state.sites.map((site) => {
+      return(
+        <div className='price'>
+          {site.name}<br />
+          {site.url}
+        </div>
+      )
+    });
+
     return(
       <div className='book'>
-        book
+        <img src={this.props.image} />
+        <div className='name'>
+          {this.props.name}
+        </div>
+        <div className='author'>
+          {this.props.author}
+        </div>
+        {sites}
+        <button className='button' type='button' onClick={this.props.onClick}>See All</button>
       </div>
     );
   }
--- react/src/components/BookList.js
+++ react/src/components/BookList.js
@@ -8,10 +8,25 @@ class BookList extends Component {
       toggle: false
     }
     this.handleClick = this.handleClick.bind(this);
+    this.seeAll = this.seeAll.bind(this);
   }
 
   handleClick() {
     this.setState ({ toggle: true })
+    let books = document.getElementsByClassName('book');
+    for (let i = 0; i < books.length; i++) {
+      document.getElementsByClassName('book')[i].style.display = 'none';
+    }
+    document.getElementsByClassName('paginate')[0].style.display = 'none';
+  }
+
+  seeAll() {
+    this.setState ({ toggle: false })
+    let books = document.getElementsByClassName('book');
+    for (let i = 0; i < books.length; i++) {
+      document.getElementsByClassName('book')[i].style.display = 'block';
+    }
+    document.getElementsByClassName('paginate')[0].style.display = 'block';
   }
 
   render() {
@@ -21,18 +36,20 @@ class BookList extends Component {
           name={this.props.name}
           author={this.props.author}
           isbn={this.props.isbn}
+          image={this.props.image}
+          onClick={this.seeAll}
         />
       )
     } else {
       return(
-        <div onClick={this.handleClick}>
-        <img src={this.props.image} />
-        <div className='name'>
-        {this.props.name}
-        </div>
-        <div className='author'>
-        {this.props.author}
-        </div>
+        <div onClick={this.handleClick} className='book'>
+          <img src={this.props.image} />
+          <div className='name'>
+            {this.props.name}
+          </div>
+          <div className='author'>
+            {this.props.author}
+          </div>
         </div>
       );
     }
