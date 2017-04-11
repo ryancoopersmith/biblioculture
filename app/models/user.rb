807635df3d42165edbe05bf49a4a9622dcd90e76
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-04-11 13:55:48 -0400

    initialize devise

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A((\w+)|(\.))+\@[a-z]+\.[a-z]{3}\z/ }
+  validates :role, inclusion: { in: ['admin', 'member'] }
+end
