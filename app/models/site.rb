08fc7ed5cb50af226f9b84dea9aa44f15d376622
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-04-11 15:23:06 -0400

    Fix seeding issue

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,7 +1,6 @@
 class Site < ApplicationRecord
   validates :name, presence:true
   validates :url, presence:true
-  validates :book_id, presence:true
 
   belongs_to :book
   has_many :books, through: :locations
