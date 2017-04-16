495a1560ddd110ede90e6807798520b76890161f
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-04-16 16:00:52 -0400

    Use multiple chat rooms

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,44 +1,38 @@
-var messages = $('#messages');
-if (messages.length > 0) {
-  var messagesToBottom = function() {
-    return messages.scrollTop(messages.prop("scrollHeight"));
-  };
-
-  messagesToBottom();
-  App.room = App.cable.subscriptions.create({
-    channel: "RoomsChannel",
-    room_id: messages.data('room-id'),
-
-    connected: function() {
-      // Called when the subscription is ready for use on the server
-    },
-
-    disconnected: function() {
-      // Called when the subscription has been terminated by the server
-    },
-
-    received: function(data) {
-      messages.append(data['message']);
-      messagesToBottom();
-    },
-
-    send_message: function(message, room_id) {
-      return this.perform('send_message', {
-        message: message,
-        room_id: room_id
-      });
-    }
-  });
-
-  $('#new_message').submit(function(e) {
-    var $this, textarea;
-    $this = $(this);
-    textarea = $this.find('#message_body');
-    if ($.trim(textarea.val()).length > 0) {
-      App.global_chat.send_message(textarea.val(), messages.data('chat-room-id'));
-      textarea.val('');
-    }
-    e.preventDefault();
-    return false;
-  });
-}
+jQuery(document).on('turbolinks:load', function() {
+  var messages, messages_to_bottom;
+  messages = $('#messages');
+  if ($('#messages').length > 0) {
+    messages_to_bottom = function() {
+      return messages.scrollTop(messages.prop("scrollHeight"));
+    };
+    messages_to_bottom();
+    App.global_room = App.cable.subscriptions.create({
+      channel: "RoomsChannel",
+      room_id: messages.data('room-id')
+    }, {
+      connected: function() {},
+      disconnected: function() {},
+      received: function(data) {
+        messages.append(data['message']);
+        return messages_to_bottom();
+      },
+      send_message: function(message, room_id) {
+        return this.perform('send_message', {
+          message: message,
+          room_id: room_id
+        });
+      }
+    });
+    return $('#new_message').submit(function(e) {
+      var $this, textarea;
+      $this = $(this);
+      textarea = $this.find('#message_body');
+      if ($.trim(textarea.val()).length > 1) {
+        App.global_room.send_message(textarea.val(), messages.data('room-id'));
+        textarea.val('');
+      }
+      e.preventDefault();
+      return false;
+    });
+  }
+});
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,27 @@
 class Message < ApplicationRecord
   after_create_commit { MessageBroadcastJob.perform_later(self) }
 
-  validates :body, presence: true, length: { maximum: 500 }
+  validates :body, presence: true, length: { minimun: 1, maximum: 500 }
 
   belongs_to :user
   belongs_to :chat_room
 
   def timestamp
-    created_at.strftime('%H:%M:%S %d %B %Y')
+    day = created_at.strftime('%B %d, %Y')
+    hour = created_at.strftime('%H').to_i
+    minute = created_at.strftime('%M').to_i
+    morning = true
+    if hour >= 12
+      morning = false
+      unless hour == 12
+        hour -= 12
+      end
+    end
+    if morning
+      time = "#{hour}:#{minute} AM, #{day}"
+    else
+      time = "#{hour}:#{minute} PM, #{day}"
+    end
+    time
   end
 end
