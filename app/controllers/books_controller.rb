de3b35f70be15efe5ba38365f4338d8afa22c269
Ryan Coopersmith <ryancoopersmith1@gmail.com>
2017-04-28 15:09:37 -0400

    Add search for multiple books at once functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,44 +26,51 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
+    @books = []
+    10.times { @books << Book.new }
   end
 
   def create
-    @book = Book.new
+    @books = []
+    params['books'].each do |book|
+      if book['name'] != '' || book['isbn_10'] != '' || book['isbn_13'] != ''
+        @book = Book.new
+        if book['name'] != ''
+          google_spider = GoogleSpider.new(name: books_params(book)['name'])
+        elsif book['isbn_10'] != ''
+          google_spider = GoogleSpider.new(isbn_10: books_params(book)['isbn_10'])
+        elsif book['isbn_13'] != ''
+          google_spider = GoogleSpider.new(isbn_13: books_params(book)['isbn_13'])
+          @book.isbn_13 = google_spider.isbn_13
+        else
+          flash[:notice] = 'You must supply either the title, ISBN-10 or ISBN-13'
+          render action: 'new'
+        end
 
-    if books_params['name'] != ''
-      google_spider = GoogleSpider.new(name: books_params['name'])
-    elsif books_params['isbn_10'] != ''
-      google_spider = GoogleSpider.new(isbn_10: books_params['isbn_10'])
-    elsif books_params['isbn_13'] != ''
-      google_spider = GoogleSpider.new(isbn_13: books_params['isbn_13'])
-      @book.isbn_13 = google_spider.isbn_13
-    else
-      flash[:notice] = 'You must supply either the title, ISBN-10 or ISBN-13'
-      render action: 'new'
-    end
+        google_spider.find_book
 
-    google_spider.find_book
+        @book.name = google_spider.name.force_encoding('iso8859-1').encode('utf-8')
+        @book.author = google_spider.author.force_encoding('iso8859-1').encode('utf-8')
+        @book.image = google_spider.image
+        @book.isbn_10 = google_spider.isbn_10
 
-    @book.name = google_spider.name.force_encoding('iso8859-1').encode('utf-8')
-    @book.author = google_spider.author.force_encoding('iso8859-1').encode('utf-8')
-    @book.image = google_spider.image
-    @book.isbn_10 = google_spider.isbn_10
-
-    if @book.save
-      google_spider.scrape_prices_and_sites.each do |site|
-        book_site = Site.create(name: site[0])
-        book_price = Price.create(price: site[1], book: @book)
-        SitePrice.create(site: book_site, price: book_price)
-        Location.create(site: book_site, book: @book)
+        if @book.save
+          google_spider.scrape_prices_and_sites.each do |site|
+            book_site = Site.create(name: site[0])
+            book_price = Price.create(price: site[1], book: @book)
+            SitePrice.create(site: book_site, price: book_price)
+            Location.create(site: book_site, book: @book)
+          end
+          @books << @book
+        else
+          flash[:notice] = 'There was an error finding the book'
+          @book = Book.new
+          render action: 'new'
+        end
       end
-      redirect_to @book
-    else
-      flash[:notice] = 'There was an error finding the book'
-      @book = Book.new
-      render action: 'new'
     end
+    binding.pry
+    # Redirect to show page for all books added here
   end
 
   def edit
@@ -72,7 +79,9 @@ class BooksController < ApplicationController
   def update
   end
 
-  def books_params
-    params.require(:book).permit(:name, :isbn_10, :isbn_13)
+  private
+
+  def books_params(book_params)
+    book_params.permit(:name, :isbn_10, :isbn_13)
   end
 end
